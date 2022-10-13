67129c8afcef8c60e0b12d5542bcbaebf65f0211
ahmedma7m0ud <113228437+ahmedma7m0ud@users.noreply.github.com>
2022-10-13 19:30:36 +0200

    after adding edit page.

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth  import views as auth_views
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 
 
     # path('login/', auth_views.LoginView.as_view(), name='login'),
